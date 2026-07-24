7ad6945ccf540e53e18c974f905b819d8ea36ecd
Dante <officemendante@gmail.com>
2026-07-24 23:55:22 +0530

    fix(ui): assign MainWindow property on startup to prevent premature WPF app exit

--- viper/src/Viper.UI/App.xaml.cs
+++ viper/src/Viper.UI/App.xaml.cs
@@ -9,24 +9,37 @@ namespace Viper.UI
         {
             base.OnStartup(e);
 
+            AppDomain.CurrentDomain.UnhandledException += (s, args) =>
+            {
+                System.IO.File.WriteAllText(System.IO.Path.Combine(System.IO.Path.GetTempPath(), "viper_ui_error.txt"), args.ExceptionObject.ToString());
+            };
+
+            DispatcherUnhandledException += (s, args) =>
+            {
+                System.IO.File.WriteAllText(System.IO.Path.Combine(System.IO.Path.GetTempPath(), "viper_ui_error.txt"), args.Exception.ToString());
+            };
+
             var args = Environment.GetCommandLineArgs();
             
             if (Array.Exists(args, a => a.Equals("setup", StringComparison.OrdinalIgnoreCase)))
             {
                 // First-run setup: create Master Password + App Unlock Password
                 var setupWindow = new SetupWindow();
+                MainWindow = setupWindow;
                 setupWindow.Show();
             }
             else if (Array.Exists(args, a => a.Equals("settings", StringComparison.OrdinalIgnoreCase)))
             {
                 // Settings screen: manage protected apps (requires Master Password auth first)
                 var settingsWindow = new SettingsWindow();
+                MainWindow = settingsWindow;
                 settingsWindow.Show();
             }
             else
             {
                 // Default: Lock screen (with optional "locked" arg for Lockdown Mode)
                 var mainWindow = new MainWindow();
+                MainWindow = mainWindow;
                 mainWindow.Show();
             }
         }
